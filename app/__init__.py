208a46cd91d62f3da340a87660657d25f494321e
Jhaelin toliver <jhaelintoliver@Jhaelins-iMac.hitronhub.home>
2020-06-23 13:47:50 -0500

    Added Files

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5f54db74d2df04cf82525db3fbd5f70a55b2450a6e250ef8fd2644e48acfa50c'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'danger'
+
+
+from app import routes
